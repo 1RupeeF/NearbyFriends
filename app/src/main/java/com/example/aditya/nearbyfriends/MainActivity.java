f3f73b427b3ffd4f21966f607853eb4f4febb595
adityajn <adityajn105@gmail.com>
2016-10-01 18:11:43 +0530

    place picker added buttons added(manual/auto) snackbars added

--- app/src/main/java/com/example/aditya/nearbyfriends/MainActivity.java
+++ app/src/main/java/com/example/aditya/nearbyfriends/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.aditya.nearbyfriends;
-
-import android.*;
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,13 +23,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback{
     private GoogleMap gMap;
     private int REQUEST_PERMISSIONS_KEY=1;
+    private int PLACE_PICKER_REQUEST_CODE=123;
     private LocationManager lm;
     private String[] permissions=new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -32,6 +45,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         SupportMapFragment smf=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
         smf.getMapAsync(this);
+        findViewById(R.id.auto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSetCurrentLocation();
+            }
+        });
+        findViewById(R.id.manual).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSetManualLocation();
+            }
+        });
     }
 
     @Override
@@ -51,6 +76,67 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         else{
 
+        }
+    }
+
+    public void onSetCurrentLocation(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            final Geocoder gc=new Geocoder(this, Locale.getDefault());
+            final Location location=lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Snackbar.make(findViewById(R.id.activity_main),"Your location is set to current location",Snackbar.LENGTH_LONG)
+                    .setAction("Address", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            try {
+                                String address=gc.getFromLocation(location.getLatitude(),location.getLongitude(),1).get(0).getAddressLine(0);
+                                Snackbar.make(findViewById(R.id.activity_main),address,Snackbar.LENGTH_LONG).show();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    })
+                    .setActionTextColor(Color.GREEN)
+                    .show();
+        }
+        else{
+            Snackbar.make(findViewById(R.id.activity_main),"Failed to get Current Location",Snackbar.LENGTH_LONG)
+                    .setAction("Enter Manually", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            onSetManualLocation();
+                        }
+                    })
+                    .setActionTextColor(Color.BLUE)
+                    .show();
+        }
+    }
+
+    public void onSetManualLocation(){
+        try {
+            Intent intent= new PlacePicker.IntentBuilder().build(this);
+            startActivityForResult(intent,PLACE_PICKER_REQUEST_CODE);
+
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        final Intent sdata=data;
+        if(requestCode==PLACE_PICKER_REQUEST_CODE){
+            if(resultCode==RESULT_OK){
+                Snackbar.make(findViewById(R.id.activity_main),"Your new location is set by you",Snackbar.LENGTH_LONG)
+                        .setAction("Address", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String address=PlacePicker.getPlace(sdata,getApplicationContext()).getAddress().toString();
+                                Snackbar.make(findViewById(R.id.activity_main),address,Snackbar.LENGTH_LONG).show();
+                            }
+                        })
+                        .setActionTextColor(Color.GREEN)
+                        .show();
+            }
         }
     }
 }
