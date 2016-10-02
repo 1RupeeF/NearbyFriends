e2ae579b1ee1a49c625f2f44085de32ec44d6fb3
adityajn <adityajn105@gmail.com>
2016-10-02 12:45:21 +0530

    Butterknife added

--- app/src/main/java/com/example/aditya/nearbyfriends/SignUp.java
+++ app/src/main/java/com/example/aditya/nearbyfriends/SignUp.java
@@ -3,8 +3,6 @@ package com.example.aditya.nearbyfriends;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
-import android.view.View;
 import android.support.design.widget.TextInputEditText;
 import android.widget.Toast;
 
@@ -27,7 +25,6 @@ public class SignUp extends AppCompatActivity {
 
     @OnClick(R.id.submit)
     public void submitClick(){
-        uname=(TextInputEditText)findViewById(R.id.username);
         if(!uname.getText().equals("")) {
             String s=uname.getText().toString();
             pref.setUsername(uname.getText().toString());
